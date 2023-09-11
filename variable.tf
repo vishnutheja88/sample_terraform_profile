22d95638a1eb1da4435f54345e3fa91fb609901d
vishnutheja88 <vishnutheja88@gmail.com>
2023-09-11 12:27:31 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  type = string
+}
+variable "aws_profile" {
+  type = string
+}
+variable "myip" {
+
+}
+variable "environment" {
+  type = string
+}
+
+variable "aws_ami" {
+  type = string
+}
+variable "aws_instance_bastion" {
+  type = string
+}
+variable "key_name" {
+  type = string
+}
+variable "aws_instance_jenkins" {
+  type = string
+}
+variable "aws_instance_application" {
+  type = string
+}
+
